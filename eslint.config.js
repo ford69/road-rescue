876348c6b8ae21119985e5e943a18d58cfd07ce8
ford69 <cliffordmanu7@gmail.com>
2026-09-02 17:02:31 +0000

    npm audit

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'backend/**', 'node_modules/**'] },
+  { ignores: ['dist', 'backend/**', 'node_modules/**', '**/*.d.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['src/**/*.{ts,tsx}'],
